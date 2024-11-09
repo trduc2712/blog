9bb1f1e55cad282afa9e542c77c75bb703484c01
trduc <trduc2712@gmail.com>
2024-11-09 19:25:52 +0700

    Cập nhật các API mới

--- server/index.js
+++ server/index.js
@@ -35,33 +35,61 @@ db.connect((err) => {
         return;
     }
     console.log('Kết nối thành công');
-})
+});
 
 app.use(express.json());
 
+app.post('/api/sign-up', (req, res) => {
+    const { username, password, name } = req.body;
+    db.query('SELECT * FROM users WHERE username = ?', [username], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'Lỗi máy chủ' });
+        }
+        if (results.length > 0) {
+            return res.status(400).json({ error: 'Tên người dùng đã tồn tại' });
+        }
+        db.query('INSERT INTO users (username, password, name) VALUES (?, ?, ?)', [username, password, name], (err, results) => {
+            if (err) {
+                return res.status(500).json({ error: 'Lỗi máy chủ' });
+            }
+            return res.status(201).json({ message: 'Đăng ký thành công' });
+        });
+    });
+});
+
 app.post('/api/login', (req, res) => {
     const { username, password } = req.body;
-    db.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, results) => {
+    db.query('SELECT * FROM users WHERE username = ?', [username], (err, results) => {
         if (err) {
             return res.status(500).json({ error: 'Lỗi máy chủ'});
         }
         if (results.length == 0) {
-            return res.status(404).json({ error: 'Sai tài khoản hoặc mật khẩu' });
+            return res.status(404).json({ error: 'Người dùng này không tồn tại' });
         }
-        const user = results[0];
-        req.session.user = {
-            id: user.id,
-            name: user.name
-        }
-        res.json({
-            message: 'Đăng nhập thành công',
-            user: {
+        db.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, results) => {
+            if (err) {
+                return res.status(500).json({ error: 'Lỗi máy chủ'});
+            }
+            if (results.length == 0) {
+                return res.status(404).json({ error: 'Sai tài khoản hoặc mật khẩu' });
+            }
+            const user = results[0];
+            req.session.user = {
                 id: user.id,
                 name: user.name,
+                avatar: user.avatar
             }
+            res.json({
+                message: 'Đăng nhập thành công',
+                user: {
+                    id: user.id,
+                    name: user.name,
+                    avatar: user.avatar
+                }
+            });
         });
-    })
-})
+    });
+});
 
 app.get('/api/user', (req, res) => {
     if (req.session.user) {
@@ -82,7 +110,37 @@ app.get('/api/logout', (req, res) => {
         res.clearCookie('connect.sid');
         res.json({ message: 'Đăng xuất thành công' });
     });
-})
+});
+
+app.get('/api/posts', (req, res) => {
+    db.query(`
+            SELECT 
+                posts.id, 
+                posts.title, 
+                posts.image AS post_image, 
+                posts.slug,
+                posts.created_at,
+                posts.updated_at,
+                users.name AS user_name,
+                users.avatar AS user_avatar,
+                categories.name AS category_name
+            FROM posts
+            JOIN users ON posts.user_id = users.id
+            JOIN categories ON posts.category_id = categories.id
+        `, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'Lỗi máy chủ'});
+        }
+        if (results.length == 0) {
+            return res.status(404).json({ error: 'Không có bài viết nào' });
+        }
+        const posts = results;
+        res.json({
+            message: 'Lấy bài viết thành công',
+            posts: posts
+        });
+    });
+});
 
 app.listen(port, () => {
     console.log(`Máy chủ đang chạy tại: http://localhost:${port}`)
